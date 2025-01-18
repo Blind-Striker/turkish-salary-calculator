952c19b1ca6d6e2869188b6b8cf3e9adff7d1fcf
Deniz İrgin <denizirgin@gmail.com>
2025-01-18 14:25:36 +0300

    chore: remove obsolete test library TurkEmployCalc.Tests

--- test/TurkEmployCalc.Tests/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-﻿// Global using directives
-
-global using System.Diagnostics.CodeAnalysis;
-global using System.IO.Abstractions;
-global using System.IO.Abstractions.TestingHelpers;
-global using FluentAssertions;
-global using TurkHRSolutions.TurkEmployCalc.Services.Parameter;
-global using TurkHRSolutions.TurkEmployCalc.Tests.TestData;
-global using Xunit;
